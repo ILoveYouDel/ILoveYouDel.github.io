4b7af0a893ddfeeb99572dab0e94f8ad5db6d667
ILoveYouDel <yoyo23devwork@gmail.com>
2025-09-16 14:59:35 -0400

    Add files via upload

---
+++ Libro/libro.js
@@ -0,0 +1,11 @@
+let currentPage = 0
+const pages = document.querySelectorAll('.page')
+function flipPage(){
+    if(currentPage<pages.length){
+        pages[currentPage].classList.add('flipped')
+        currentPage++;
+    } else {
+        pages.forEach(p => p.classList.remove('flipped'));
+        currentPage = 0;
+    }
+}
